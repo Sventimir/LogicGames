55bc9811d7e63546109e396a5b043c3fa7290b70
Sventimir <svantimir@gmail.com>
2014-04-05 23:33:44 +0000

    Functions for counting objects guessed correctly and those misplaced in a guess.

--- src/MasterMind.hs
+++ src/MasterMind.hs
@@ -1,3 +1,22 @@
 module MasterMind where
 
+import Data.List
 
+data Answer = Answer { correct :: Int, misplaced :: Int, wrong :: Int }
+
+-- TODO: Rewrite enabling tail recursion optimisation.
+extractCorrect :: (Eq a) => [a] -> [a] -> ([a], [a], [a])
+extractCorrect [] guess = ([], [], guess)
+extractCorrect pattern [] = ([], pattern, [])
+extractCorrect (p:pat) (g:guess)
+    | p == g = (p : newCorrect, newPat, newGuess)
+    | otherwise = (newCorrect, p : newPat, g : newGuess)
+        where
+        (newCorrect, newPat, newGuess) = extractCorrect pat guess
+
+countMisplaced :: (Eq a) => [a] -> [a] -> Int
+countMisplaced [] _ = 0
+countMisplaced _ [] = 0
+countMisplaced (p:pat) guess
+    | p `elem` guess = 1 + countMisplaced pat (delete p guess)
+    | otherwise = 0 + countMisplaced pat guess
--- src/testSuite.hs
+++ src/testSuite.hs
@@ -2,10 +2,24 @@ import System.Exit
 import Test.HUnit
 import MasterMind
 
-testEnv = TestCase $ do
-    1 @=? 1
+testCorrectness = TestLabel "Test function counting correct guesses." $
+        TestCase $ do
+    ("", "", "") @=? extractCorrect "" ""
+    ("", "A", "F") @=? extractCorrect "A" "F"
+    ("X", "", "") @=? extractCorrect "X" "X"
+    ("B", "ADEF", "CEAD") @=? extractCorrect "ABDEF" "CBEAD"
+    ("HWDP", "X", "Y") @=? extractCorrect "XHWDP" "YHWDP"
+    ("C", "ABDE", "EDBA") @=? extractCorrect "ABCDE" "EDCBA"
 
-testSuite = TestList [testEnv]
+testMisplacement = TestLabel "Test function counting misplaced guesses." $
+        TestCase $ do
+    0 @=? countMisplaced "" ""
+    0 @=? countMisplaced "ABCDE" "XF"
+    2 @=? countMisplaced "EDCBA" "ABHJK"
+    1 @=? countMisplaced "BFG" "ABC"
+    3 @=? countMisplaced "ABXFZ" "XXFAA"
+
+testSuite = TestList [testCorrectness, testMisplacement]
 
 main = do
     counts <- runTestTT $ TestList [testSuite]
